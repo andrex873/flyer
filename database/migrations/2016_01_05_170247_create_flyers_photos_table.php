a0348a6e7864a882b9e841a246214c8009b880b4
Misael A. Mendez J <Misael.MendezJuanias@wnco.com>
2016-01-05 13:27:52 -0600

    Video 6

---
+++ database/migrations/2016_01_05_170247_create_flyers_photos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFlyersPhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flyers_photos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('flyer_id')->unsigned();
+            $table->string('photo');
+            $table->timestamps();
+            $table->foreign('flyer_id')->references('id')->on('flyers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('flyers_photos');
+    }
+}
